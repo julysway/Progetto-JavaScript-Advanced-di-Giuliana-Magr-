da08f2a66e15b8b7eb846b68f4f233246ba4ea9c
djswalterix <walter45hoxha@gmail.com>
2023-10-24 10:41:38 +0200

    img sistemate

--- webpack.config.js
+++ webpack.config.js
@@ -4,57 +4,54 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
-
-const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : "style-loader";
-
-
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : "style-loader";
 
 const config = {
-    entry: "./src/js/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-    },
-    devServer: {
-        open: true,
-        host: "localhost",
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
-    
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        /*ho modificato questa parte in modo da non rinominare l'immagine */
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "img/[name][ext]",
+        } /*ho modificato questa parte in modo da non rinominare l'immagine */,
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: "babel-loader",
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler,"css-loader"],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: "asset/resource",
-                generator: {
-                    filename: "img/[name][ext]",
-                },
-            },
-        ],
-    },  
-    };
+  },
+};
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = "production";
-        
-        config.plugins.push(new MiniCssExtractPlugin());
-        
-        
-    } else {
-        config.mode = "development";
-    }
-    return config;
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new MiniCssExtractPlugin());
+  } else {
+    config.mode = "development";
+  }
+  return config;
 };
