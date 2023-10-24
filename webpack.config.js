f823175c3046f83728488623070bc90be1a4eafb
GiulianaMagri <giulianamagri91@gmail.com>
2023-10-24 09:24:01 +0200

    .

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: "asset/resource",
                 generator: {
-                    filename: "imag/[name][ext]",
+                    filename: "img/[name][ext]",
                 },
             },
         ],
